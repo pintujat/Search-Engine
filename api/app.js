037b1ab643944f86362bea30312ae92603b577e4
Pintu Jat <pintujat@Ajits-MacBook-Air.local>
2021-05-14 21:54:18 -0400

    chorus setup

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 var express = require('express'), 
 path = require('path'),
 indexRouter = require('./routes/index'),
+ cors = require("cors"),
 app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
